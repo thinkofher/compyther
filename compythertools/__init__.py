34ad31c1f5ca0d2c9d8c1f432c6a150034041b5f
Beniamin Dudek <piaskupiasku@gmail.com>
2018-09-29 12:43:41 +0200

    solving import problems

---
+++ compythertools/__init__.py
@@ -0,0 +1,3 @@
+from compythertools.funcs import replace_word
+
+__all__ = [replace_word]
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from compythertools.funcs import replace_word
+from compythertools import replace_word
 
 replace_word('sample\\sample.tex', '<:Beniamin:>', 'Marek świętokrzyski')
 replace_word('output.tex', '<:sierść:>', 'Ćma sobie lata')
