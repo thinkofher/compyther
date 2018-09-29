7a9bf76a8da35f8a8fbac89be04a83af8c8bfb6f
Beniamin Dudek <piaskupiasku@gmail.com>
2018-09-29 12:47:56 +0200

    modifying replace_word by adding "newfile" variable to it

--- compythertools/funcs.py
+++ compythertools/funcs.py
@@ -2,12 +2,12 @@ import os
 import sys
 
 
-def replace_word(infile, old_word, new_word):
+def replace_word(infile, newfile, old_word, new_word):
     if not os.path.isfile(infile):
         print("Error on replace_word, not a regular file: "+infile)
         sys.exit(1)
 
     fileRead = open(infile, 'r', encoding='utf-8').read()
-    fileWrite = open('output.tex', 'w', encoding='utf-8')
+    fileWrite = open(newfile, 'w', encoding='utf-8')
     temporaryFile = fileRead.replace(old_word, new_word)
     fileWrite.write(temporaryFile)
