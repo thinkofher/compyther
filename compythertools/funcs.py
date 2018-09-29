808ea74bc63c9e0754343f201c7b4acbcedabf77
Beniamin Dudek <piaskupiasku@gmail.com>
2018-09-29 15:54:19 +0200

    editing comments nothing important

--- compythertools/funcs.py
+++ compythertools/funcs.py
@@ -8,10 +8,11 @@ def replace_word(infile, newfile, old_word, new_word):
         print("Error on replace_word, not a regular file: "+infile)
         sys.exit(1)
 
-    # Check this too: fileRead = open(infile, 'r', encoding='utf-8').read()
+    # Check this too if utf-8 fails:
+    # fileRead = open(infile, 'r', encoding='utf-8').read()
     fileRead = open(infile, 'r').read()
 
-    # Check this too: fileWrite = open(newfile, 'w', encoding='utf-8')
+    # fileWrite = open(newfile, 'w', encoding='utf-8')
     fileWrite = open(newfile, 'w')
     temporaryFile = fileRead.replace(old_word, new_word)
     fileWrite.write(temporaryFile)
