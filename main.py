8628ef1a75acc23fb9d478b0601ec4240d8b178f
Beniamin Dudek <piaskupiasku@gmail.com>
2018-09-29 11:41:45 +0200

    makind code more readable

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def replace_word(infile, old_word, new_word):
         print("Error on replace_word, not a regular file: "+infile)
         sys.exit(1)
 
-    f1 = open(infile, 'r', encoding='utf-8').read()
-    f2 = open('output.tex', 'w', encoding='utf-8')
-    m = f1.replace(old_word, new_word)
-    f2.write(m)
+    fileRead = open(infile, 'r', encoding='utf-8').read()
+    fileWrite = open('output.tex', 'w', encoding='utf-8')
+    temporaryFile = fileRead.replace(old_word, new_word)
+    fileWrite.write(temporaryFile)
 
 
 replace_word('sample\\sample.tex', '<:Beniamin:>', 'Marek świętokrzyski')
