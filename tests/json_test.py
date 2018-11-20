48ce90d35010679698991d752d3b08733b869b9f
thinkofher <piaskupiasku@gmail.com>
2018-11-20 22:37:29 +0100

    created unit test

---
+++ tests/json_test.py
@@ -0,0 +1,32 @@
+import unittest
+from pathlib import Path
+
+from compythertools.funcs import replace_with_json
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_jsonfile(self):
+
+        self._sampleInput = Path('tests/samplefiles/sample_input.txt')
+        self._sampleOutput = Path('tests/samplefiles/sample_output.txt')
+        self._sampleJson = Path('tests/samplefiles/sample_data.json')
+        self._newFile = Path('tests/samplefiles/test_output.txt')
+
+        replace_with_json(
+            self._sampleInput,
+            self._newFile,
+            self._sampleJson
+        )
+
+        with open(self._sampleOutput, 'r') as file1, \
+                open(self._newFile, 'r') as file2:
+
+            self.assertEquals(
+                file1.read(),
+                file2.read()
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
